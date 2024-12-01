35ca40235edcb7a32c6dcbc01be675ad558d7814
Giorgio <gciotti29@gmail.com>
2024-12-01 13:51:52 +0100

    Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import "Navbar.css";
+    function Navbar() {
+    return (
+        <ul>
+            <li>
+                <a href="#">Ciao</a>
+            </li>
+            <li>
+                <a href="#">Ciao</a>
+            </li>
+            <li>
+                <a href="#">Ciao</a>
+            </li>
+            <li>
+                <a href="#">Ciao</a>
+            </li>
+            <li>
+                <a href="#">Ciao</a>
+            </li>
+        </ul>
+    );
+}
+
+export default Navbar;
